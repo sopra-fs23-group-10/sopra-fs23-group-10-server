5765dee735f2202a79289eb320f67c029465293c
sandrin <sandrin@hunkeler.ws>
2023-04-11 12:49:13 +0200

    bugfix in String formating #58

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/WebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/WebSocketController.java
@@ -55,13 +55,13 @@ public class WebSocketController {
     public void register(@Payload String userId) {
         Long id = Long.parseLong(userId);
         userService.setOnline(userService.searchUserById(id));
-        System.out.printf("User with userID: %s has logged IN", userId);
+        System.out.printf("User with userID: %s has logged IN\n", userId);
     }
 
     @MessageMapping("/unregister")
     public void unregister(@Payload String userId) {
         Long id = Long.parseLong(userId);
         userService.setOffline(userService.searchUserById(id), id);
-        System.out.printf("User with userID: %s has logged OUT", userId);
+        System.out.printf("User with userID: %s has logged OUT\n", userId);
     }
 }
