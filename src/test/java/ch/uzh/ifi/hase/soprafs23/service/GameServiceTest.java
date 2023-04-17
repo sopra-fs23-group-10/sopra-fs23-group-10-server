497e1880c0c7aa9c60205cd97e84cedd8071c1be
Patrick Widmer <patrickwidmer@gmx.ch>
2023-04-17 17:15:08 +0200

    added tests for #45

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/GameServiceTest.java
@@ -4,24 +4,35 @@ package ch.uzh.ifi.hase.soprafs23.service;
 import ch.uzh.ifi.hase.soprafs23.constant.Category;
 import ch.uzh.ifi.hase.soprafs23.constant.ModeType;
 import ch.uzh.ifi.hase.soprafs23.constant.QuizType;
+import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs23.entity.Game;
+import ch.uzh.ifi.hase.soprafs23.entity.Question;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.handler.GameMap;
+import ch.uzh.ifi.hase.soprafs23.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GameServiceTest {
 
     @InjectMocks
     private GameService gameService;
+
+    @Mock
+    private UserService userService;
 
     private Game prepTestTextDuelGame;
 
@@ -53,6 +64,48 @@ class GameServiceTest {
         Field instance = GameMap.class.getDeclaredField("instance");
         instance.setAccessible(true);
         instance.set(null, null);
+    }
+
+    @Test
+    public void getQuestion_validInput_success() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testUsername");
+        user.setPassword("testPassword");
+        user.setPoints(2L);
+        user.setEmail("email@email.com");
+        user.setProfilePicture("testUsername");
+        user.setToken("1");
+        user.setStatus(UserStatus.ONLINE);
+
+        when(userService.searchUserById(Mockito.any())).thenReturn(user);
+
+        Question receivedQuestion = gameService.getQuestion(Category.MUSIC, prepTestTextDuelGame.getId());
+        assertNotNull(receivedQuestion);
+        assertEquals(receivedQuestion.getClass(), Question.class);
+    }
+
+    @Test
+    public void getQuestion_invalidID_throwsException() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testUsername");
+        user.setPassword("testPassword");
+        user.setPoints(2L);
+        user.setEmail("email@email.com");
+        user.setProfilePicture("testUsername");
+        user.setToken("1");
+        user.setStatus(UserStatus.ONLINE);
+
+        when(userService.searchUserById(Mockito.any())).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "User with specified ID does not exist."));
+
+        boolean tester = false;
+        try {
+            Question receivedQuestion = gameService.getQuestion(Category.MUSIC, prepTestTextDuelGame.getId());
+        } catch (Exception e) {
+            tester = true;
+        }
+        assertTrue(tester);
     }
 
     @Test
@@ -147,7 +200,7 @@ class GameServiceTest {
         } catch (Exception e) {
             tester = true;
         }
-        
+
         assertTrue(tester);
     }
 
