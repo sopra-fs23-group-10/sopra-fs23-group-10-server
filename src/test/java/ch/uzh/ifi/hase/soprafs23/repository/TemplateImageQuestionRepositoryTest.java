49e3a47f6996bf18997cdf1970085288b1ebc1c5
Patrick Widmer <patrickwidmer@gmx.ch>
2023-05-10 17:41:00 +0200

    trying to fix deployment

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/TemplateImageQuestionRepositoryTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/TemplateImageQuestionRepositoryTest.java
@@ -1,16 +1,11 @@
 package ch.uzh.ifi.hase.soprafs23.repository;
 
-import ch.uzh.ifi.hase.soprafs23.entity.TemplateImageQuestion;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 class TemplateImageQuestionRepositoryTest {
@@ -23,6 +18,6 @@ class TemplateImageQuestionRepositoryTest {
   @Test
   void count_success(){
     long cnt = templateImageQuestionRepository.count();
-    assertEquals(4L,cnt);
+    assertEquals(4, cnt);
   }
 }
