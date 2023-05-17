eb32f107d35a5c8c05b551ac88c1071fbaff339f
sandrin <sandrin@hunkeler.ws>
2023-05-17 10:55:27 +0200

    bugfix for users not getting notification that both users have answered

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameController.java
@@ -122,10 +122,11 @@ public class GameController {
   public Map<String, String> answerQuestion(@PathVariable long gameId, @RequestBody AnswerDTO answerDTO, @RequestHeader("token") String token) {
       userService.verifyToken(token);
       Answer answer = DTOMapper.INSTANCE.convertUserAnswerDTOtoEntity(answerDTO);
+      String correctAnswer = gameControllerService.answerQuestion(answer);
       if (gameControllerService.bothAnswered(gameId, answer.getQuestionId())){
         webSocketController.informUsersBothAnswered(gameId);
       }
-      return Collections.singletonMap("correctAnswer", gameControllerService.answerQuestion(answer));
+      return Collections.singletonMap("correctAnswer", correctAnswer);
   }
 
   //TODO: Is this even called in frontend?
